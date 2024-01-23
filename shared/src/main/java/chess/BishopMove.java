89aaa423f70d0aece8372d3f624907e141b50a69
Hyrum Davis <hd284@byu.edu>
2024-01-23 00:20:42 -0700

    BishopalmostWorking(phase0)

--- shared/src/main/java/chess/BishopMove.java
+++ shared/src/main/java/chess/BishopMove.java
@@ -1,12 +1,100 @@
 package chess;
 
-import java.util.Collection;
+import java.util.*;
 
 public class BishopMove{
     public BishopMove() {
 
     }
-    public Collection<ChessMove> CheckMove(ChessBoard board, ChessPosition BeginPos) {
-        return null;
+    public Collection<ChessMove> CheckMove(ChessBoard board, ChessPosition beginPos) {
+        Set<ChessMove> moves = new HashSet<ChessMove>();
+        boolean proceed = true;
+        ChessPosition current;
+        current = beginPos;
+        while (proceed) {
+            if (current.getRow() > 0 && current.getColumn() > 0) {
+                current = new ChessPosition(current.getRow() -1,current.getColumn() -1);
+                if (board.getPiece(current) == null) {
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else if (board.getPiece(current).getTeamColor() != board.getPiece(beginPos).getTeamColor()) {
+                    proceed = false;
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else {
+                    proceed = false;
+                }
+            } else proceed = false;
+        }
+        proceed = true;
+        current = beginPos;
+        while (proceed) {
+            if (current.getRow() > 0 && current.getColumn() < 7) {
+                current = new ChessPosition(current.getRow() -1,current.getColumn() +1);
+                if (board.getPiece(current) == null) {
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else if (board.getPiece(current).getTeamColor() != board.getPiece(beginPos).getTeamColor()) {
+                    proceed = false;
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else {
+                    proceed = false;
+                }
+            }
+            else proceed = false;
+        }
+        proceed = true;
+        current = beginPos;
+        while (proceed) {
+            if (current.getRow() < 7 && current.getColumn() > 0) {
+                current = new ChessPosition(current.getRow() +1,current.getColumn() -1);
+                if (board.getPiece(current) == null) {
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else if (board.getPiece(current).getTeamColor() != board.getPiece(beginPos).getTeamColor()) {
+                    proceed = false;
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else {
+                    proceed = false;
+                }
+            }
+            else proceed = false;
+        }
+        proceed = true;
+        current = beginPos;
+        while (proceed) {
+            if (current.getRow() < 7 && current.getColumn() < 7) {
+                current = new ChessPosition(current.getRow() +1,current.getColumn() +1);
+                if (board.getPiece(current) == null) {
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else if (board.getPiece(current).getTeamColor() != board.getPiece(beginPos).getTeamColor()) {
+                    proceed = false;
+                    moves.add(new ChessMove(beginPos,current,null));
+                }
+                else {
+                    proceed = false;
+                }
+            }
+            else proceed = false;
+        }
+        return moves;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
