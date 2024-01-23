f7ef22c9c7a68b798e047d61d838ff3bc9d928ba
Hyrum Davis <hd284@byu.edu>
2024-01-23 12:32:57 -0700

    QueenPass(phase0)

--- shared/src/main/java/chess/QueenMove.java
+++ shared/src/main/java/chess/QueenMove.java
@@ -1,4 +1,18 @@
 package chess;
 
+import java.awt.desktop.QuitEvent;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class QueenMove {
+    QueenMove() {
+
+    }
+
+    public Collection<ChessMove> CheckMove(ChessBoard board, ChessPosition beginPos) {
+        Set<ChessMove> moves = (Set<ChessMove>) new RookMove().CheckMove(board, beginPos);
+        moves.addAll(new BishopMove().CheckMove(board, beginPos));
+        return moves;
+    }
 }
