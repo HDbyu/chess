426937d796ceb1b6acf5ef9bfa7d303940851914
HDbyu <hd284@byu.edu>
2024-07-18 16:04:28 -0600

    init(Phase3)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,22 +1,19 @@
 package server;
 
-import dataAccess.MemoryData;
 import spark.*;
-
-import java.util.UUID;
 
 public class Server {
 
-    private MemoryData Data;
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
-        //Spark.delete("/db")
-        //Spark.post("/user", (req, res) -> RegisterHandler().read(req, res));
+
         Spark.awaitInitialization();
         return Spark.port();
     }
@@ -26,4 +23,3 @@ public class Server {
         Spark.awaitStop();
     }
 }
-//UUID
