17055a7061bc6f80e228dcd2340b227b7e53fdd6
HDbyu <hd284@byu.edu>
2024-07-22 17:50:55 -0600

    RegisterService writtten(Phase3)

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -3,6 +3,12 @@ package service;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import requestresult.RegisterRequest;
+import requestresult.RegisterResult;
+
+import java.util.UUID;
 
 public class RegisterService {
     MemoryUserDAO userDAO;
@@ -15,5 +21,20 @@ public class RegisterService {
         this.userDAO = userDAO;
     }
 
-    public void register(String username, String password) {}
+    public RegisterResult register(RegisterRequest request) {
+        UserData user = null;
+        String authToken = null;
+        try {
+            user = userDAO.getUser(request.username());
+
+            if(user == null) {
+                userDAO.createUser(new UserData(request.username(), request.password(), request.email()));
+                authToken = UUID.randomUUID().toString();
+                authDAO.createAuth(new AuthData(authToken, request.username()));
+            }
+        } catch (Exception e) {
+            return new RegisterResult(null, null, "Error: bad request");
+        }
+        return new RegisterResult(request.username(), authToken, null);
+    }
 }
--- shared/src/main/java/requestresult/RegisterResult.java
+++ shared/src/main/java/requestresult/RegisterResult.java
@@ -2,5 +2,5 @@ package requestresult;
 
 import model.AuthData;
 
-public record RegisterResult(String username, AuthData auth, String error) {
+public record RegisterResult(String username, String authToken, String message) {
 }
