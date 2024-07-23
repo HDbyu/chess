0dc81777cc551a52d1dff2a4c35ba16152b1615f
HDbyu <hd284@byu.edu>
2024-07-22 19:24:32 -0600

    regHandler done(Phase3)

--- server/src/main/java/Handlers/RegisterHandler.java
+++ server/src/main/java/Handlers/RegisterHandler.java
@@ -5,6 +5,8 @@ import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
 import requestresult.RegisterRequest;
+import requestresult.RegisterResult;
+import service.RegisterService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -24,7 +26,15 @@ public class RegisterHandler implements Route {
     public Object handle(Request request, Response response) throws Exception {
         var gson = new Gson();
         RegisterRequest req = gson.fromJson(request.body(), RegisterRequest.class);
-        response.status(200);
-        return gson.toJson();
+        RegisterService service = new RegisterService(gameDAO, authDAO, userDAO);
+        RegisterResult result = service.register(req);
+        if (result.message().equals("Error: bad request")) {
+            response.status(400);
+        } else if (result.message().equals("Error: already taken")) {
+            response.status(403);
+        } else if (result.message().equals("Error: database error")) {
+            response.status(500);
+        } else response.status(200);
+        return gson.toJson(result);
     }
 }
