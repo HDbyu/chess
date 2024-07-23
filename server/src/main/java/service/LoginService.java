4cb6d9c993ed47d8bbd6bde5b3543669210bc8c2
HDbyu <hd284@byu.edu>
2024-07-22 22:14:34 -0600

    LoginService done(Phase3)

---
+++ server/src/main/java/service/LoginService.java
@@ -0,0 +1,39 @@
+package service;
+
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import requestresult.*;
+
+
+import java.util.UUID;
+
+public class LoginService {
+    MemoryUserDAO userDAO;
+    MemoryGameDAO gameDAO;
+    MemoryAuthDAO authDAO;
+
+    public LoginService(MemoryGameDAO gameDAO, MemoryAuthDAO authDAO, MemoryUserDAO userDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+
+    public LoginResult login(LoginRequest request) {
+        UserData user = null;
+        String authToken = null;
+        try {
+            user = userDAO.getUser(request.username());
+
+            if(user.password().equals(request.password())) {
+                authToken = UUID.randomUUID().toString();
+                authDAO.createAuth(new AuthData(authToken, request.username()));
+            } else return new LoginResult(null, null, "Error: unauthorized");
+        } catch (Exception e) {
+            return new LoginResult(null, null, "Error: database error");
+        }
+        return new LoginResult(request.username(), authToken, null);
+    }
+}
