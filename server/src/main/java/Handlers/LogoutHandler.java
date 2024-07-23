f69e43b67b3997c1038635e2356cbb0b2b76f13c
HDbyu <hd284@byu.edu>
2024-07-22 23:08:00 -0600

    LogoutHandler done(Phase3)

---
+++ server/src/main/java/Handlers/LogoutHandler.java
@@ -0,0 +1,35 @@
+package Handlers;
+
+import com.google.gson.Gson;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import requestresult.*;
+import service.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class LogoutHandler implements Route {
+    private MemoryAuthDAO authDAO;
+
+    public LogoutHandler(MemoryAuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        var gson = new Gson();
+        LogoutRequest req = gson.fromJson(request.body(), LogoutRequest.class);
+        LogoutService service = new LogoutService(authDAO);
+        LogoutResult result = service.logout(req);
+        if (result.message().equals("Error: unauthorized")) {
+            response.status(401);
+        } else if (result.message().equals("Error: database error")) {
+            response.status(500);
+        } else response.status(200);
+        if(result.message() == null) {
+            return "";
+        }
+        return gson.toJson(result);
+    }
+}
