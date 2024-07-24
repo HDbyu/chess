eb905136e2eae128a0695272254bee0d41f838b8
HDbyu <hd284@byu.edu>
2024-07-23 18:06:00 -0600

    Handler fix(Phase3)

--- server/src/main/java/Handlers/ListGamesHandler.java
+++ server/src/main/java/Handlers/ListGamesHandler.java
@@ -20,7 +20,7 @@ public class ListGamesHandler implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         var gson = new Gson();
-        ListGamesRequest req = gson.fromJson(request.headers("authorization"), ListGamesRequest.class);
+        ListGamesRequest req = new ListGamesRequest(request.headers("authorization"));
         ListGamesService service = new ListGamesService(gameDAO, authDAO);
         ListGamesResult result = service.listGames(req);
         if (result.message() == null) {
