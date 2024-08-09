691c09c9eeef6b7fb25ad78b025abf5e306c1c82
HDbyu <hd284@byu.edu>
2024-08-09 16:18:53 -0600

    websocket and makemovecommand (Phase6)

--- server/src/main/java/handlers/WebSocketHandler.java
+++ server/src/main/java/handlers/WebSocketHandler.java
@@ -5,18 +5,22 @@ import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
 
 @WebSocket
 public class WebSocketHandler {
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) {
+    public void onMessage(Session session, String message) throws Exception {
         Gson gson = new Gson();
         UserGameCommand gameCommand = gson.fromJson(message, UserGameCommand.class);
         if (gameCommand.getCommandType() == UserGameCommand.CommandType.MAKE_MOVE) {
-            gameCommand = gson.fromJson(message, UserGameCommand.class);
+            gameCommand = gson.fromJson(message, MakeMoveCommand.class);
         }
+        session.getRemote().sendString("WebSocket response: " + message);
     }
 
     private void connect() {
